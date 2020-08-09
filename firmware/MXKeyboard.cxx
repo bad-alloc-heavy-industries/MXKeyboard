883812ba1d20d746ccfd57aee4670d4bf7efb76b
dx-mon <dx-mon@users.sourceforge.net>
2020-08-09 21:28:17 +0100

    Implemented some test pattern generating code for the LEDs on the keyboard

--- firmware/MXKeyboard.cxx
+++ firmware/MXKeyboard.cxx
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #include <avr/builtins.h>
 #include "MXKeyboard.hxx"
+#include "led.hxx"
+
+uint8_t redValue{0};
+bool incRed{true};
 
 void run()
 {
@@ -9,5 +13,19 @@ void run()
 	ledInit();
 
 	while (true)
-		continue;
+	{
+		if (incRed)
+			++redValue;
+		else
+			--redValue;
+
+		if (redValue == 0)
+			incRed = true;
+		else if (redValue == 255)
+			incRed = false;
+
+		ledSetValue(0, redValue, incRed ? 1 : 0, 255 - redValue);
+		ledLatch();
+		__builtin_avr_delay_cycles(266660UL);
+	}
 }
