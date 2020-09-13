334ffaa8f61fb3b0cc18fd7336c7d732ceb84c00
dx-mon <dx-mon@users.sourceforge.net>
2020-09-13 22:06:23 +0100

    Trying variations on a theme to get working ELPM code that doesn't also invoke UB in its operation

--- firmware/flash.hxx
+++ firmware/flash.hxx
@@ -3,8 +3,8 @@
 
 #include <cstring>
 #include <array>
-#include <type_traits>
 #include <avr/io.h>
+//#include <avr/pgmspace.h>
 
 template<typename T> struct flash_t final
 {
@@ -14,36 +14,62 @@ private:
 public:
 	constexpr flash_t(const T value) noexcept : value_{value} { }
 
-	constexpr operator T() const noexcept
+	operator T() const noexcept
 	{
-		if (std::is_constant_evaluated())
-			return value_;
-		else
-		{
-			T result{};
-			std::array<std::byte, sizeof(T)> buffer{};
-			const uint8_t z{RAMPZ};
+		T result{};
+		const uint8_t x{RAMPX};
+		const uint8_t z{RAMPZ};
 
-			__asm__ __volatile__(R"(
-				ldi r30, lo8(%[value])
-				ldi r31, hi8(%[value])
-				ldi r16, hh8(%[value])
-				out 0x3B, r16
-				)" : : [value] "m" (value_) : "r16", "r30", "r31"
-			);
+		__asm__(R"(
+			mov r26, %A[result]
+			mov r27, %B[result]
+			out 0x39, %C[result]
+			mov r30, %A[value]
+			mov r31, %B[value]
+			out 0x3B, %C[value]
+			ldi r17, %[count]
+			cpi r17, 0
+loop%=:
+			breq loopDone%=
+			elpm r16, Z+
+			st X+, r16
+			dec r17
+			rjmp loop%=
+loopDone%=:
+			)" : [result] "=g" (result) : [value] "p" (&value_), [count] "I" (sizeof(T)) :
+				"r16", "r17", "r26", "r27", "r30", "r31"
+		);
 
-			for (auto &byte : buffer)
-			{
-				__asm__ __volatile__(R"(
-					elpm %[byte], Z+
-					)" : [byte] "=r" (byte)
-				);
-			}
+		RAMPZ = z;
+		RAMPX = x;
+		return result;
+	}
+};
 
-			RAMPZ = z;
-			memcpy(&result, buffer.data(), buffer.size());
-			return result;
-		}
+template<> struct flash_t<uint16_t> final
+{
+private:
+	uint16_t value_;
+
+public:
+	constexpr flash_t(const uint16_t value) noexcept : value_{value} { }
+	//operator uint16_t() const noexcept { return pgm_read_word_far(&value_); }
+	operator uint16_t() const noexcept
+	{
+		uint16_t result{};
+		__asm__ __volatile__(R"(
+			in r0, %[rampz]
+			out %[rampz], %C[value]
+			movw r30, %[value]
+			elpm %A[result], Z+
+			elpm %B[result], Z
+			out %[rampz], r0
+			)" :
+			[result] "=r" (result) :
+			[value] "p" (&value_), [rampz] "I" (_SFR_IO_ADDR(RAMPZ)) :
+			"r30", "r31"
+		);
+		return result;
 	}
 };
 
