9aa824819bed0ff9a0f6f42db6c3510ea6ee5743
dx-mon <dx-mon@users.sourceforge.net>
2020-11-22 21:25:44 +0000

    Implemented Aki's smarter mask generator

---
+++ firmware/include/mask.hxx
@@ -0,0 +1,21 @@
+#ifndef MASK__HXX
+#define MASK__HXX
+
+#include <cstdint>
+#include <cstddef>
+#include <type_traits>
+#include <limits>
+
+template<typename T, std::size_t offset, std::size_t count>
+	constexpr std::enable_if_t<std::is_integral_v<T> && !std::is_signed_v<T>  &&
+    	offset < std::numeric_limits<T>::digits &&
+    	(offset + count) < std::numeric_limits<T>::digits, T>
+	genMask() noexcept
+{
+    T mask_base{};
+    for (std::size_t idx{}; idx < count; ++idx)
+        mask_base |= (1U << (offset + idx));
+    return mask_base;
+}
+
+#endif /*MASK__HXX*/
