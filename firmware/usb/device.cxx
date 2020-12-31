13fa7d03d580ac25f4cb302bb03c897f3cbc8364
dx-mon <dx-mon@users.sourceforge.net>
2020-12-31 22:30:47 +0000

    Begun implementing the USB device driver

--- firmware/usb/device.cxx
+++ firmware/usb/device.cxx
@@ -8,7 +8,27 @@ void usbHandleStatusCtrlEP() noexcept;
 
 namespace usb::device
 {
+	setupPacket_t packet;
 
+	answer_t handleStandardRequest() noexcept
+	{
+		const auto &epStatus{epStatusControllerIn[0]};
+
+		switch (packet.request)
+		{
+			case request_t::setAddress:
+				usbState = deviceState_t::addressing;
+				return {response_t::zeroLength, nullptr, 0};
+			case request_t::getDescriptor:
+				return handleGetDescriptor();
+		}
+
+		return {response_t::unhandled, nullptr, 0};
+	}
+}
+
+namespace usb::device
+{
 	void handleControlPacket() noexcept
 	{
 	}
