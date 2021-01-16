e65297c1a5a5fb1bc879657d05d9aa4c1acd5d70
dx-mon <dx-mon@users.sourceforge.net>
2021-01-16 22:13:56 +0000

    Fixed the clock and oscillator bringup issues in debug builds

--- firmware/system.cxx
+++ firmware/system.cxx
@@ -14,7 +14,7 @@ void oscInit()
 	PORTR.DIRCLR = 0x01;
 	OSC.XOSCCTRL = OSC_FRQRANGE_12TO16_gc | OSC_XOSCSEL_EXTCLK_gc;
 	// Enable the external osc
-	OSC.CTRL = 0x09;
+	OSC.CTRL |= 0x09;
 	while (!(OSC.STATUS & 0x08))
 		continue;
 	CCP = CCP_IOREG_gc;
@@ -24,8 +24,6 @@ void oscInit()
 	CLK.PSCTRL = CLK_PSADIV_1_gc | CLK_PSBCDIV_1_1_gc;
 	CCP = CCP_IOREG_gc;
 	CLK.CTRL = CLK_SCLKSEL_XOSC_gc;
-	CCP = CCP_IOREG_gc;
-	CLK.LOCK = 1;
 
 	// Disable the internal 2MHz RC osc, but also enable the 32MHz one.
 	OSC.CTRL = 0x0A;
