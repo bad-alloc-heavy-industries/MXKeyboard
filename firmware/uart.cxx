dd2a1834c941d89aff2819d99df8467af9705842
dx-mon <dx-mon@users.sourceforge.net>
2021-01-16 22:17:31 +0000

    Initialise the debug UART to 2MBaud

--- firmware/uart.cxx
+++ firmware/uart.cxx
@@ -12,32 +12,64 @@
  * COPI_BLUE = PC7
  * COPI_RED = PD3
  *
+ * DBG_RX = PE6
+ * DBG_TX = PE7
+ *
  * USARTC0 (PC1 & 3)
  * USARTC1 (PC5 & 7)
  * USARTD0 (PD1 & 3)
+ * USARTE1 (PE6 & 7)
  */
 
-void uartInitOne(USART_t &uart)
+enum class usartMode_t
 {
-	// UDORD is MSB first, UCPHA is leading edge
-	uart.CTRLC = USART_CMODE_MSPI_gc | USART_PMODE_DISABLED_gc |
-		USART_CHSIZE_8BIT_gc;
-	// Enable TX
-	uart.CTRLB = 0x08;
-	// Clear both BSEL and BSCALE to get as close as possible to no clock division
-	uart.BAUDCTRLA = 0;
-	uart.BAUDCTRLB = 0;
+	UART,
+	USRT,
+	IrDA,
+	MSPI
+};
+
+template<usartMode_t mode = usartMode_t::MSPI> constexpr void uartInitOne(USART_t &usart) noexcept
+{
+	if constexpr (mode == usartMode_t::MSPI)
+	{
+		// UDORD is MSB first, UCPHA is leading edge
+		usart.CTRLC = USART_CMODE_MSPI_gc | USART_PMODE_DISABLED_gc |
+			USART_CHSIZE_8BIT_gc;
+		// Enable TX
+		usart.CTRLB = 0x08;
+		// Clear both BSEL and BSCALE to get as close as possible to no clock division
+		usart.BAUDCTRLA = 0;
+		usart.BAUDCTRLB = 0;
+	}
+	else if (mode == usartMode_t::UART)
+	{
+		// Set the USART into async mode, 8n1 w/ no parity and 1 stop bit.
+		usart.CTRLC = USART_CMODE_ASYNCHRONOUS_gc | USART_PMODE_DISABLED_gc |
+			USART_CHSIZE_8BIT_gc;
+		// Enable TX, RX, and CLX2X
+		usart.CTRLB = 0x1C;
+		// Clear both BSEL and BSCALE to get as close as possible to no clock division
+		// As Fper is 16MHz, this puts us at 2Baud having enabled CLX2X on this peripheral.
+		usart.BAUDCTRLA = 0;
+		usart.BAUDCTRLB = 0;
+	}
 }
 
-void uartInit()
+void uartInit() noexcept
 {
 	uartInitOne(USARTC0);
 	uartInitOne(USARTC1);
 	uartInitOne(USARTD0);
+	uartInitOne<usartMode_t::UART>(USARTE1);
 	// Make PC1, 3, 5 and 7 outputs
 	PORTC.DIRSET = 0xAA;
 	// Make PD 1 & 3 ouputs
 	PORTD.DIRSET = 0x0A;
+	// Force PE 6 to be an input
+	PORTE.DIRCLR = 0x40;
+	// Make PE 7 an output
+	PORTE.DIRSET = 0x80;
 }
 
 void uartWrite(USART_t &uart, const uint8_t data)
