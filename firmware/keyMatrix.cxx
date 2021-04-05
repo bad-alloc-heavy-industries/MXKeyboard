c03b1085edf7194a5665730e97070d1bea0aabf6
dx-mon <dx-mon@users.sourceforge.net>
2021-04-05 18:41:47 +0100

    Implemented the key to LED mappings

--- firmware/keyMatrix.cxx
+++ firmware/keyMatrix.cxx
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #include <cstdint>
+#include <array>
 #include <avr/cpufunc.h>
 #include "MXKeyboard.hxx"
 #include "interrupts.hxx"
+#include "keyMatrix.hxx"
 #include "mask.hxx"
+#include "indexedIterator.hxx"
+#include "led.hxx"
 
 /*!
  * The column signals are on Port A, bits 0-4
@@ -12,7 +16,8 @@
 
 constexpr static const auto columnMask{genMask<std::uint8_t, 0U, 5U>()};
 constexpr static const auto rowMask{genMask<std::uint8_t, 0U, 6U>()};
-uint16_t scanIterations{};
+
+std::array<keyState_t, keyCount> keyStates{};
 
 void keyInit() noexcept
 {
@@ -30,56 +35,43 @@ void keyInit() noexcept
 	PORTF.DIRCLR = rowMask;
 	PORTF.OUTCLR = ~rowMask;
 
-	TCD0.CTRLA = TC_CLKSEL_DIV1_gc; // Use fPER directly
+	TCD0.CTRLA = TC_CLKSEL_DIV4_gc; // Use fPER/4
 	TCD0.CTRLB = TC_WGMODE_NORMAL_gc;
 	TCD0.CTRLE = TC_BYTEM_NORMAL_gc;
 	TCD0.INTCTRLA = TC_OVFINTLVL_HI_gc;
 	TCD0.CTRLFCLR = 0x0FU;
 	TCD0.CTRLFSET = TC_CMD_UPDATE_gc;
-	TCD0.PERBUF = 20000; // 2MHz / 1kHz = 20000
+	TCD0.PERBUF = 10000; // 1MHz / 1kHz = 5000
 	TCD0.CNT = 0;
 
-	scanIterations = 0;
+	// Pull the initial key state information from flash
+	for (const auto &[i, keyState] : utility::indexedIterator_t{keyStates})
+	{
+		const key_t key = keys[i];
+		keyState->ledIndex = key.ledIndex;
+	}
 }
 
 void keyIRQ() noexcept
 {
-	if (!scanIterations)
+	for (uint8_t column{0}; column < 21; ++column)
 	{
-		for (uint8_t i{0}; i < 21; ++i)
+		PORTA.OUT = column;
+		// This waits for the propergation delays in the 3-to-8 decoders so the PORTF read is valid
+		for (volatile uint8_t wait{0}; wait < 2; ++wait)
+			continue;
+		const auto pressStates = PORTF.IN;
+		for (uint8_t row{0}; row < 6; ++row)
 		{
-			PORTA.OUT = i;
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
-			_NOP();
+			const bool keyPressed = (pressStates >> row) & 1U;
+			const auto &key = keyStates[(column * 6) + row];
+			if (key.ledIndex == 255)
+				continue;
+
+			if (keyPressed)
+				ledSetValue(key.ledIndex, 0x00, 0xFF, 0x00);
+			else
+				ledSetValue(key.ledIndex, 0x1F, 0x1F, 0xFF);
 		}
 	}
-	++scanIterations;
-	if (scanIterations >= 1000)
-		scanIterations = 0;
 }
