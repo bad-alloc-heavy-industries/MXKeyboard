c196927a1b88cad27447427f93edffb6cbad1e0b
dx-mon <dx-mon@users.sourceforge.net>
2021-04-17 22:05:32 +0100

    Fixed the setup of TCC0

--- firmware/timer.cxx
+++ firmware/timer.cxx
@@ -3,12 +3,12 @@
 
 void timerInit(TC0_t &timer)
 {
-	timer.CTRLA = TC_CLKSEL_DIV8_gc;
+	timer.CTRLA = TC_CLKSEL_DIV4_gc;
 	timer.CTRLB = TC_WGMODE_NORMAL_gc;
 	timer.CTRLE = TC_BYTEM_NORMAL_gc;
 	timer.INTCTRLA = TC_OVFINTLVL_LO_gc;
 	timer.CTRLFCLR = 0x0FU;
 	timer.CTRLFSET = TC_CMD_UPDATE_gc;
-	timer.PERBUF = 33333;
+	timer.PER = 33333;
 	timer.CNT = 0;
 }
